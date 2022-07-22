dff81ac43d1b9f7d24669ddc18923411a31320e0
Leck Tang <achrysaetos@yahoo.com>
2022-07-22 16:53:46 -0500

    Add viewport and tools

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,82 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { FrontendIModelsAccess } from '@itwin/imodels-access-frontend';
+	import { IModelsClient } from '@itwin/imodels-client-management';
+	import { BrowserAuthorizationCallbackHandler } from '@itwin/browser-authorization';
+	import type { BrowserAuthorizationClientConfiguration } from '@itwin/browser-authorization';
+	import {
+		BentleyCloudRpcManager,
+		IModelReadRpcInterface,
+		IModelTileRpcInterface
+	} from '@itwin/core-common';
+	import { IModelApp, LocalExtensionProvider } from '@itwin/core-frontend';
+	// import { PresentationRpcInterface } from '@itwin/presentation-common';
+	import AuthClient from '../utils/clients/Authorization';
+	import ConfigClient from '../utils/clients/Configuration';
+	import type { ViewerConfiguration } from '../utils/clients/Configuration';
+	import Viewport from '../components/Viewport.svelte';
+	import '@bentley/icons-generic-webfont/dist/bentley-icons-generic-webfont.css';
+	import { onMount } from 'svelte';
+
+	const signIn = async (authConfig: BrowserAuthorizationClientConfiguration) => {
+		try {
+			await BrowserAuthorizationCallbackHandler.handleSigninCallback(authConfig.redirectUri);
+		} catch (error) {
+			console.error(error);
+		}
+
+		AuthClient.initialize(authConfig);
+		const authClient = AuthClient.client;
+
+		return new Promise<boolean>((resolve, reject) => {
+			authClient.onAccessTokenChanged.addOnce((token: string) => resolve(token !== ''));
+			authClient.signIn().catch((err: any) => reject(err));
+		});
+	};
+
+	const initialize = async (config: ViewerConfiguration) => {
+		const iModelsClient = new IModelsClient();
+
+		await IModelApp.startup({
+			authorizationClient: AuthClient.client,
+			hubAccess: new FrontendIModelsAccess(iModelsClient),
+			rpcInterfaces: [IModelReadRpcInterface],
+			mapLayerOptions: {
+				BingMaps: {
+					key: 'key',
+					value: config.map?.bingKey ?? ''
+				}
+			}
+		});
+		BentleyCloudRpcManager.initializeClient(
+			{
+				uriPrefix: 'https://api.bentley.com',
+				info: { title: 'imodel/rpc', version: '' }
+			},
+			[IModelReadRpcInterface, IModelTileRpcInterface]
+		);
+	};
+
+	/**
+	 * App startup
+	 */
+	let iTwinId = '';
+	let iModelId = '';
+	onMount(async () => {
+		await ConfigClient.initialize();
+		const config = ConfigClient.config;
+		await signIn(config.authorization);
+		console.log('Signed in...');
+		await initialize(config);
+		console.log('Initialized...');
+		iTwinId = config.iTwinId;
+		iModelId = config.iModelId;
+	});
+</script>
+
+<main>
+	{#if iTwinId == '' || iModelId == ''}
+		<span id="viewer-loading-span">Initializing and signing in...</span>
+	{:else}
+		<Viewport {iTwinId} {iModelId} />
+	{/if}
+</main>
