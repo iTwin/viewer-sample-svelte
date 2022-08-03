3d8212ce61174efae25ffb1dc7e9cfafc6cdd720
Leck Tang <achrysaetos@yahoo.com>
2022-08-03 17:02:44 -0500

    Add house extension

--- packages/svelte-app/src/routes/index.svelte
+++ packages/svelte-app/src/routes/index.svelte
@@ -15,7 +15,8 @@ See LICENSE.md in the project root for license terms and full copyright notice.
   } from "@itwin/core-common";
   import { IModelApp, LocalExtensionProvider } from "@itwin/core-frontend";
   import { PresentationRpcInterface } from "@itwin/presentation-common";
-  import SelectTool from "@itwin/select-tool-extension-sample";
+  // import SelectTool from "@itwin/select-tool-extension-sample";
+  import MyExtension from "my-extension";
   import AuthClient from "../utils/clients/Authorization";
   import ConfigClient from "../utils/clients/Configuration";
   import type { ViewerConfiguration } from "../utils/clients/Configuration";
@@ -74,7 +75,8 @@ See LICENSE.md in the project root for license terms and full copyright notice.
    */
   const addExtensions = async () => {
     await IModelApp.extensionAdmin.addExtension(
-      new LocalExtensionProvider(SelectTool)
+      // new LocalExtensionProvider(SelectTool)
+      new LocalExtensionProvider(MyExtension)
     );
   };
 
