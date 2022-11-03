6d465d47c0da6d2c0045658b7d466b11adf120e5
Arun George <11051042+aruniverse@users.noreply.github.com>
2022-11-03 10:30:08 -0400

    Publish (#4)
    Co-authored-by: Arun George <aruniverse@users.noreply.github.com>

---
+++ packages/svelte-viewer/vite.config.js
@@ -0,0 +1,35 @@
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import { sveltekit } from "@sveltejs/kit/vite";
+import * as dotenv from "dotenv-flow";
+import { dirname } from "path";
+
+dotenv.config();
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  plugins: [
+    sveltekit(),
+  ],
+  define: {
+    "process.env": process.env,
+    __dirname: { dirname },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+};
+
+export default config;
